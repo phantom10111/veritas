3f2dcacb8c5e6b58ba4c890986ad2436dd5d6208
Michal Seweryn <michalsew@gmail.com>
2014-01-08 11:09:20 +0100

    submit command

--- src/webserver/commands.cpp
+++ src/webserver/commands.cpp
@@ -4,13 +4,38 @@
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/ssl.hpp>
 #include <pqxx/pqxx> 
+#include <cstdlib>
 #include <map>
 
 
 void submit(pqxx::result::tuple &user, 
-            ssl_socket& stream, 
+            ssl_socket& socket, 
             pqxx::connection &conn){
-    stream.write("OK");
+    std::string contestname, shortname, extension;
+    socket.read(contestname).read(shortname);
+    conn.prepare("user contests", 
+        "      SELECT contestid        " 
+        "        FROM participations   "
+        "NATURAL JOIN contests         "
+        "       WHERE userid = $1      "
+        "         AND contestname = $2 ")
+        ("integer")("varchar", pqxx::prepare::treat_string);
+    int userid = user["userid"].as<int>();
+    pqxx::work txn(conn);
+    pqxx::result contests = txn.prepared("user contests")(userid)
+                                                    (contestname).exec();
+    txn.commit();
+    if(contests.empty()){
+        socket.write("ERROR NO SUCH PARTICIPATION");
+    }
+    socket.read(extension);
+    std::string filename = 
+        user["login"].as<std::string>() + 
+        shortname + 
+        "." + 
+        extension;
+    socket.readfile(filename);
+    socket.write("OK");
 }
 
 std::map<std::string, command_handler> command_handlers(){
